b74710145fe33e25c1623e547a5e75db2adf4498
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2023-07-20 01:34:09 -0400

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
-
+  const [items, setItems] = useState(itemData);
   const [isDarkMode, setIsDarkMode] = useState(false)
 
   function handleDarkModeClick() {
@@ -20,7 +20,7 @@ function App() {
         <h2>Shopster</h2>
         <button onClick={handleDarkModeClick}>Dark Mode</button>
       </header>
-      <ShoppingList items={itemData} />
+      <ShoppingList items={items} />
     </div>
   );
 }
--- src/components/Item.js
+++ src/components/Item.js
@@ -3,20 +3,23 @@ import React, {useState} from "react";
 function Item({ name, category }) {
   const [isInCart, setIsInCart] = useState(false)
 
-  function handleAddtoCartClick() {
-    setIsInCart(isInCart => !isInCart)
+  function handleAddToCartClick() {
+    setIsInCart((isInCart) => !isInCart);
   }
 
   return (
-    <li className= {isInCart ? "in-cart" : "" }>
+    <li className={isInCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
       <button 
-        onClick={handleAddtoCartClick} 
-        className={isInCart ? "remove" : "add"}> {isInCart ? "Remove From" : "Add To"} cart
+        className={isInCart ? "remove" : "add"}
+        onClick={handleAddToCartClick} 
+        > 
+        {isInCart ? "Remove From" : "Add To"} Cart
       </button>
     </li>
   );
 }
 
 export default Item;
+
